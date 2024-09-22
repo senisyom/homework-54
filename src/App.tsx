fbf6a8b50a0d1f94d1ece6311de6b67b17ff174c
Begmatov Almaz <lolkek13@inbox.ru>
2024-09-22 11:24:20 +0600

    Создал функцию по созданию объектов

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+
   
-}
+  const createObject = () => {
+    let array = [];
+    for (let i = 0; i < 36; i++) {
+      array.push({
+        hasItem: false,
+        clicked: false,
+      });
+    }
+    console.log(array);
+    let randomObject = Math.floor(Math.random() * array.length);
+     console.log("Случайный индекс:", randomObject); 
 
-export default App
+   array[randomObject] = {
+     hasItem: true,
+     clicked: false,
+   };
+    console.log("Массив после изменения случайного объекта:", array);
+
+    return array;
+  };
+
+  const [items, setItems] = useState(createObject());
+
+  const createItems = () => {
+    const newItems = createObject() 
+    setItems(newItems)
+  }
+
+  return (
+    <div className="container">
+      <div className="kekw">Kek</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/components/OneBlock.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './OneBlock.css'
+
+interface BlockProps{
+
+}
+
+const OneBlock: React.FC<BlockProps> = () => {
+    return (
+        <div className="one-block">
+        </div>
+    )
+}
